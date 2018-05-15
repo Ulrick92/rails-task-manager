be8fcc1c6e9785f30a980277e0e8afc223b12bbb
Ulrick Labeau <ulrick.labeau@gmail.com>
2018-05-15 16:05:21 +0200

    exercice rails task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Read all tasks
+  get "/tasks", to: "tasks#index"
+  # # Create one task (2 requests)
+  get "/tasks/new", to: "tasks#new"
+  post "/tasks", to: "tasks#create"
+  # # Read one task
+  get "/tasks/:id", to: "tasks#show", as: "task"
+  # # Update one task
+  get "/tasks/:id/edit", to: "tasks#edit", as: "edit"
+  patch "/tasks/:id", to: "tasks#update"
+  # # Delete one task
+  delete "/tasks/:id", to: "tasks#destroy"
 end
